50cc4442ec331140f8b043e7a32c1e80f36f0cb8
Gabriela Caruso <gabriela.caruso@yahoo.com.br>
2021-06-10 01:53:55 -0300

    add pause

---
+++ boss-final/Assets/Scripts/Pause.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject panel;
+    public static bool pausado;
+
+    public AudioMixer mixer;
+    public Slider musicaSlider;
+    public Slider efeitosSlider;
+
+    private float musicaVol;
+    private float efeitosVol;
+
+    void Awake()
+    {
+        panel.SetActive(false);
+        pausado = false;
+
+        mixer.GetFloat("music", out musicaVol);
+        musicaSlider.value = musicaVol;
+        mixer.GetFloat("effects", out efeitosVol);
+        efeitosSlider.value = efeitosVol;
+    }
+
+    public void ActivatePause()
+    {
+        Time.timeScale = 0f;
+        panel.SetActive(true);
+        pausado = true;
+    }
+
+    public void DeactivatePause()
+    {
+        Time.timeScale = 1f;
+        panel.SetActive(false);
+        pausado = false;
+    }
+
+    public void MenuButton()
+    {
+        Time.timeScale = 1f;
+        pausado = false;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void SetVolumeMusica(float volume)
+    {
+        mixer.SetFloat("music", volume);
+        Debug.Log(volume);
+    }
+
+    public void SetVolumeEfeitos(float volume)
+    {
+        mixer.SetFloat("effects", volume);
+        Debug.Log(volume);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            if (pausado == false)
+            {
+                ActivatePause();
+            } else
+            {
+                DeactivatePause();
+            }
+        }
+    }
+}
